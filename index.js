6af426708ccc6e3ec875fa76dc7ad9a1a069d0f4
Mace <maceplus@gmail.com>
2021-03-16 19:36:03 +0000

    concept set up

---
+++ index.js
@@ -0,0 +1,45 @@
+// The Martian
+// Mark Watney is stuck on Mars, with a probe (Pathfinder) that has a camera that can rotate in a full circle. NASA is on the other end, they can
+// receive pictures. So it's very easy for mark to communicate to NASA, but how do they communicate back?
+
+// Yes & No
+
+// ASCII 256
+console.log(360 / "0123456789ABCDEF".length);
+console.log("a".charCodeAt(0));
+
+// Fake CSS
+{
+  color: #FFa3df; //hexadecimal
+  color: rgb(255, 32, 22);
+}
+61 
+// BASE 2 - Binary
+1111 1111 1011 1001 1010 1011 
+// BASE 10
+1099
+// BASE 12
+0123456789&*
+// BASE 16
+0123456789ABCDEF
+
+116
+0F = 0000 1111
+F = 1111 F = 1111
+0 = 0000 0 = 0000
+
+3F = ?
+A0 FF 44 F1
+
+'abcdefgh' -> 22 , -44
+
+// PROJECT
+
+// Setup a proof of concept ascii-a-like table
+// MARK: Translate hex to string
+// MARK: Use real ascii table.
+// NASA: Tranaslate string into hex ascii values
+// NASA: Translate hex into directions for the camera.
+// Animation
+
+// http://ascii-table.com
