e8dc5eec98de5419c431a7f2bb1ae954f2ee91c8
Mace <maceplus@gmail.com>
2021-04-17 03:27:37 +0100

    passing test for invalid hex

--- src/hex.js
+++ src/hex.js
@@ -2,5 +2,11 @@ export function hexToDec(hex){
   if(hex.length !== 2){
     return NaN;
   }
+  const valid_hex = "0123456789ABCDEF"
+  for (const x of hex.split("")) {
+    if (!valid_hex.includes(x)){
+      return NaN;
+    }
+  }
   return parseInt(hex, 16);
 }
