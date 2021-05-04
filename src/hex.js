a97dbb6764c568970bee6fb478b6d122ee7b2de5
Mace <maceplus@gmail.com>
2021-05-04 16:24:03 +0100

    update hexToDec function

--- src/hex.js
+++ src/hex.js
@@ -18,4 +18,14 @@ export function hexToASCIIString(hex) {
       "Hex dosen't represent a valid string (invalid byte length)"
     );
   }
+  const bytes = [];
+  for(let i = 0; i < hex.length; i += 2) {
+    bytes.push(hex.slice(i, i +2));
+  }
+
+  const decimals = bytes.map((byte) => hexToDec(byte));
+  const message = decimals.map((decimal) => String.fromCharCode(decimal)).join("");
+
+  return message;
+
 }
